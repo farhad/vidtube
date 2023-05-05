12630f4e272600151280debf1f1dcbd9d041136d
= <ifarhadfaghihi@gmail.com>
2023-05-05 10:18:00 -0700

    fixed the folded sidebar and main container width

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector('.menu-icon');
 var sidebar = document.querySelector('.sidebar');
+var mainContainer = document.querySelector('.container');
 
 menuIcon.onclick = function () {
     sidebar.classList.toggle('small-sidebar');
+    mainContainer.classList.toggle('wide-container');
 }
